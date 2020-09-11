4dac817466c0b1ac8544dfd7c8ff1109d9f29ff6
nick-ussery <nick.ussery@gmail.com>
2020-09-11 09:03:27 -0500

    stretch

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue
 
 import random
 from ast import literal_eval
@@ -28,53 +29,44 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-visited = {}
 
-inverse_directions = {"n": "s", "s": "n",
-                      "e": "w", "w": "e"}  # easier to code backpath
+# DFS fewer moves if go all the way down and back track once deadends are hit
+
+turn_counter = 0
+
+backup_directions = {"n": "s", "s": "n",
+                     "e": "w", "w": "e"}  # easier to code backpath
 
 reverse_path = [None]  # backtrack path
-
+visited = {}
 visited[player.current_room.id] = player.current_room.get_exits()
 
+
 while len(visited) < len(room_graph):  # loop until all rooms visited
+    print(f"entered room: {player.current_room.id}")
     if player.current_room.id not in visited:  # is room new
         visited[player.current_room.id] = player.current_room.get_exits()
+        visited[player.current_room.id].remove(reverse_path[-1])
 
-        reverse_direction = reverse_path[-1]  # save backpath
-        visited[player.current_room.id].remove(reverse_direction)
+    # dead end hit when 0 exits. go back up
+    if len(visited[player.current_room.id]) == 0:
+        while len(visited[player.current_room.id]) < 1:
+            reverse_direction = reverse_path.pop()
+            traversal_path.append(reverse_direction)
+            player.travel(reverse_direction)
+    if "e" in visited[player.current_room.id]:
+        direction = visited[player.current_room.id].index("e")
+        explore = visited[player.current_room.id].pop(direction)
+    else:
+        explore = visited[player.current_room.id].pop(0)
+    print(f"taking exit {explore}")
+    traversal_path.append(explore)
+    reverse_path.append(backup_directions[explore])
 
-    # reverse up tree to go down new path
-    while len(visited[player.current_room.id]) < 1:
-        reverse_direction = reverse_path.pop()
-        traversal_path.append(reverse_direction)
-        player.travel(reverse_direction)
+    player.travel(explore)
 
-    exit_direction = visited[player.current_room.id].pop(0)
-    traversal_path.append(exit_direction)
-    reverse_path.append(inverse_directions[exit_direction])
-
-    player.travel(exit_direction)
-
-    if len(room_graph) - len(visited) == 1:  # an error is thrown if there is 1 room left. idk why
+    if len(room_graph) - len(visited) == 1:
         visited[player.current_room.id] = player.current_room.get_exits()
-
-# TRAVERSAL TEST
-visited_rooms = set()
-player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
-
-for move in traversal_path:
-    player.travel(move)
-    visited_rooms.add(player.current_room)
-
-if len(visited_rooms) == len(room_graph):
-    print(
-        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
-else:
-    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 # TRAVERSAL TEST
 visited_rooms = set()
